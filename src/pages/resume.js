cdb3ef9a71ae42b76117c1366acc4440d7a9e991
Eric Simmons <erictomlinsonsimmons@gmail.com>
2023-01-30 14:52:34 -0600

    resume format

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,10 +4,10 @@ import React, { useState } from 'react'
 function ResumePage() {
   return (
     <div className='resume'>
-<body>
-    <h1>PDF Example with iframe</h1>
-    <iframe src="https://drive.google.com/file/d/1A-emqWngTOrngtr4p_O3-m-eBMn6Gnw7/preview" width="640" height="480" allow="autoplay"></iframe>
-  </body>
+
+    
+    <iframe src="https://drive.google.com/file/d/1A-emqWngTOrngtr4p_O3-m-eBMn6Gnw7/preview" width="500" height="640" allow="autoplay"></iframe>
+
     </div>
   )
 }
