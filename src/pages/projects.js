6b2b50c7017035e2a8e20a2c40ca33f53c481eb4
Eric Simmons <erictomlinsonsimmons@gmail.com>
2023-01-30 18:32:01 -0600

    fetch github

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,15 +9,15 @@ function ProjectPage() {
     getGithub()
   }, [])
 
-  const getGithub = () => {
-    fetch('https://api.github.com/users/eric-simmons')
-      .then(res => res.json())
-      .then(json => setUserData(json))
+  const getGithub = async () => {
+    const response = await fetch('https://api.github.com/users/eric-simmons')
+    const json = await response.json()
+    setUserData(json)
   }
-  return (
+  return !!userData ? (
     <Projects
-    userData= {userData}>Projects</Projects>
-  )
+      userData={userData}>Projects</Projects>
+  ) : <p>loading..</p>
 }
 
 export default ProjectPage
