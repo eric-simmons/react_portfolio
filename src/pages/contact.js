a6f5dce2a59573064e9077a719ac3cbc4b838a39
Eric Simmons <erictomlinsonsimmons@gmail.com>
2023-01-31 13:35:17 -0600

    form validation

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import Form from '../components/form'
+import { isEmail } from '../utils/helpers'
 
 const ContactPage = (props) => {
 
-  const isEmail = (email) => {
-    /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)
-  }
 
+
+  const [errorMessage, setErrorMessage] = useState('')
   const [formState, setFormState] = useState({
     email: '',
     name: '',
@@ -14,22 +14,47 @@ const ContactPage = (props) => {
   })
 
   const handleInputs = event => {
-    let { name, value } = event.target
+    let value = event.target.value
+    let field = event.target.name
+
+    if (field === "email") {
+      let emailTest = isEmail(value)
+      if (!emailTest) {
+        setErrorMessage("Email is Invalid")
+      } else {
+        setErrorMessage('')
+      }
+    }
+
+
+    else {
+      if (!value.length) {
+        setErrorMessage(`${field} is required`)
+      } else {
+        setErrorMessage("")
+      }
+    }
+
     const updatedFormState = {
       ...formState,
-      [name]: value
+      [field]: value
     }
     setFormState(updatedFormState)
   }
 
-//clear inputs
+  //clear inputs
   const handleSubmit = event => {
     event.preventDefault()
-    setFormState({
-      email: '',
-      name: '',
-      message: ''
-    })
+    //prevent submit if there is an error
+    if (!errorMessage) {
+      setFormState({
+        email: '',
+        name: '',
+        message: ''
+      })
+    }
+
+
 
   }
 
@@ -42,6 +67,9 @@ const ContactPage = (props) => {
         handleInputs={handleInputs}
         handleSubmit={handleSubmit}
       />
+      <center>
+        {errorMessage}
+      </center>
     </>
   )
 }
